5ee24d7f68f038cc35cfe43df793de7b4570050b
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-04-23 17:57:34 +0300

    plotting the bands

--- scripts/intact_surface.py
+++ scripts/intact_surface.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from src.bands import hough
 from src.bands import two_points
 import cv2
+from src.intact_surface_detectors import StraightBandsDetector
+from src import DEBUG_COLORS
+
 
 def detect_straight_line_bands(group,img_name,is_debug=False):
     img_path = f"{intact_images_path}/group_{group}/{img_name}"
@@ -19,10 +22,21 @@ def detect_straight_line_bands(group,img_name,is_debug=False):
         points = line.sample_two_points(edge_map.shape)
         two_point_lines.append(two_points.TwoPointsLine(points[0],points[1]))
     
+    band_detector = StraightBandsDetector(hough_lines)
+    bands = band_detector.detect()
+
+
     if is_debug:
         img_ = img.copy()
-        for line in two_point_lines:
-            cv2.line(img_,line.point1,line.point2,(255,0,255),2)
+        
+        # for line in two_point_lines:
+        #     cv2.line(img_,line.point1,line.point2,(255,0,255),2)
+
+        for i,band in enumerate(bands):
+            for line in band.hough_lines:
+                points = line.sample_two_points(edge_map.shape)
+                cv2.line(img_,points[0],points[1],DEBUG_COLORS[i%len(DEBUG_COLORS)],2)
+
 
         fig, axs = plt.subplots(1,3)
         axs[0].imshow(img_preprocessed)
@@ -32,6 +46,5 @@ def detect_straight_line_bands(group,img_name,is_debug=False):
         plt.close()
     
     
-    plt.close()
 
 
--- src/intact_surface_detectors.py
+++ src/intact_surface_detectors.py
@@ -8,14 +8,14 @@ class StraightBandsDetector():
     def __init__(self,hough_lines:list[HoughLine]) -> None:
         self.hough_lines = hough_lines #
 
-    def detect(self,theta_diff=1)->list[HoughBand]:
+    def detect(self,theta_diff=1,rho_diff = 200)->list[HoughBand]:
         bands = []
         
         for line in self.hough_lines:
             is_assigned = False
 
             for band in bands:
-                if abs(band.get_theta()-line.theta)<theta_diff:
+                if abs(band.get_theta()-line.theta)<theta_diff and abs(band.get_rho()-line.rho) < rho_diff:
                     is_assigned = True
                     band.insert(line)
                     break
