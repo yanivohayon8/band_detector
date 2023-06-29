fd19f40da33994e7cf8612165655e1863e76d67f
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-06-29 13:52:33 +0300

    supress plot when not debug

--- scripts/intact_surface.py
+++ scripts/intact_surface.py
@@ -23,17 +23,20 @@ def compute_edge_map_and_segmentation(img_path,output_file_seg,output_file_edge_
 
 
 def detect_straight_line_bands(img_path,rdp_csv_path,output_dir,
-                               minimum_votes=100,theta_diff = 1, rho_diff = 200,is_debug=False):
+                               minimum_votes=100,theta_diff = 1, rho_diff = 200,min_band_width=50,is_debug=False):
     #img_path = f"{intact_images_path}/group_{group}/{img_name}"
     processor = IntactProcessor(img_path)
     img = processor.load_img()
-    img_preprocessed = processor.preprocess()
-    edge_map = processor.get_edge_map()
+    img_preprocessed,labels = processor.preprocess(n_clusters=3)
+    img_masked = processor.masked_image(2) # We have 3 clusters and empirically this has the highest variance in color (todo: compute the var?)
+    edge_map = processor.get_edge_map(img_masked)
     hough_lines = hough.detect_hough_lines(edge_map,minimum_votes=minimum_votes)
     band_detector = StraightBandsDetector(hough_lines)
-    bands = band_detector.detect(theta_diff=theta_diff,rho_diff=rho_diff)
+    bands_ = band_detector.detect(theta_diff=theta_diff,rho_diff=rho_diff)
+    bands = [band for band in bands_ if band.get_width() >= min_band_width]
+        
     bands_as_lines = []
-    
+
     for band in bands:
         line = band.get_representive_line()
         shape_shrink = (edge_map.shape[0]-1,edge_map.shape[1]-1)
@@ -115,78 +118,81 @@ def detect_straight_line_bands(img_path,rdp_csv_path,output_dir,
 
     
 
+    if not is_debug:
+        plt.ioff()
+
+    fig, axs = plt.subplots(1,3)
+    #axs[0].imshow(img_preprocessed)
+    axs[0].imshow(img_masked)
+    axs[0].set_axis_off()
+    axs[1].imshow(edge_map,cmap="gray")
+    axs[1].set_axis_off()
+    axs[2].imshow(img_with_bands)
+    axs[2].set_axis_off()
+
+    poly_x = [coord[0] for coord in polygon_coords]
+    poly_y = [coord[1] for coord in polygon_coords]
+    convex_hull_x,convex_hull_y = convex_hull.exterior.xy
+    convex_hull_x = convex_hull_x.tolist()
+    convex_hull_y = convex_hull_y.tolist()
+
+    intersec_x = []
+    intersec_y = []
+
+    for intersec in intersections:
+        _ = [vertex for vertex in list(intersec.geoms)]
+        intersec_x = intersec_x + [coord.x for coord in _]
+        intersec_y = intersec_y + [coord.y for coord in _]
+    
+    closest_x = []
+    closest_y = []
+
+    for edge_pair in intersected_edges:
+        print("###Vertices indexes of intersected edges for the next band:")
+        for edge in edge_pair:
+            xs,ys = edge.xy
+            xs = xs.tolist()
+            ys = ys.tolist()
+            closest_x= closest_x + xs 
+            closest_y= closest_y + ys
+
+            
+            for x,y in zip(xs,ys):
+                vertex_index = polygon_coords.index((x,y))
+                print(f"The index of ({x},{y}) is {vertex_index} ",end=",")
+        print()
+
+    fig.savefig(f"{output_dir}/{image_name}_pipeline.png")
+
+    fig2, ax = plt.subplots(1,1)
+    ax.set_aspect("equal",adjustable='box')
+    poly_x_plot = poly_x + [poly_x[0]]
+    poly_y_plot = poly_y + [poly_y[0]]
+    ax.plot(poly_x_plot,poly_y_plot ,color="red",label="polygon")
+
+    for i,coord in enumerate(polygon_coords):
+        ax.annotate(i,(coord[0]-1,coord[1]-1))
+
+
+
+    #fig2.gca().invert_xaxis()
+    fig2.gca().invert_yaxis()
+
+    ax.plot(convex_hull_x+[convex_hull_x[0]],convex_hull_y+[convex_hull_y[0]],color="blue",label="Convex Hull",linestyle="dashed")
+    ax.scatter(intersec_x, intersec_y, color='green', label='Intersection Points')
+    #ax.scatter(closest_x, closest_y, color='purple', label='Closest Vertices')
+    
+    for i,line in enumerate(bands_as_lines):
+        xs = [line.point1[0],line.point2[0]]
+        ys = [line.point1[1],line.point2[1]]
+        ax.plot(xs,ys,color="black",linewidth=2)
+
+    ax.legend()
+    ax.set_axis_off()
+    
+    fig2.savefig(f"{output_dir}/{image_name}_polygon.png")
+
     if is_debug:
-        fig, axs = plt.subplots(1,3)
-        axs[0].imshow(img_preprocessed)
-        axs[0].set_axis_off()
-        axs[1].imshow(edge_map,cmap="gray")
-        axs[1].set_axis_off()
-        axs[2].imshow(img_with_bands)
-        axs[2].set_axis_off()
-
-        poly_x = [coord[0] for coord in polygon_coords]
-        poly_y = [coord[1] for coord in polygon_coords]
-        convex_hull_x,convex_hull_y = convex_hull.exterior.xy
-        convex_hull_x = convex_hull_x.tolist()
-        convex_hull_y = convex_hull_y.tolist()
-
-        intersec_x = []
-        intersec_y = []
-
-        for intersec in intersections:
-            _ = [vertex for vertex in list(intersec.geoms)]
-            intersec_x = intersec_x + [coord.x for coord in _]
-            intersec_y = intersec_y + [coord.y for coord in _]
-        
-        closest_x = []
-        closest_y = []
-
-        for edge_pair in intersected_edges:
-            print("###Vertices indexes of intersected edges for the next band:")
-            for edge in edge_pair:
-                xs,ys = edge.xy
-                xs = xs.tolist()
-                ys = ys.tolist()
-                closest_x= closest_x + xs 
-                closest_y= closest_y + ys
-
-                
-                for x,y in zip(xs,ys):
-                    vertex_index = polygon_coords.index((x,y))
-                    print(f"The index of ({x},{y}) is {vertex_index} ",end=",")
-            print()
-
-        fig.savefig(f"{output_dir}/{image_name}_pipeline.png")
-
-        fig2, ax = plt.subplots(1,1)
-        ax.set_aspect("equal",adjustable='box')
-        poly_x_plot = poly_x + [poly_x[0]]
-        poly_y_plot = poly_y + [poly_y[0]]
-        ax.plot(poly_x_plot,poly_y_plot ,color="red",label="polygon")
-
-        for i,coord in enumerate(polygon_coords):
-            ax.annotate(i,(coord[0]-1,coord[1]-1))
-
-
-
-        #fig2.gca().invert_xaxis()
-        fig2.gca().invert_yaxis()
-
-        ax.plot(convex_hull_x+[convex_hull_x[0]],convex_hull_y+[convex_hull_y[0]],color="blue",label="Convex Hull",linestyle="dashed")
-        ax.scatter(intersec_x, intersec_y, color='green', label='Intersection Points')
-        #ax.scatter(closest_x, closest_y, color='purple', label='Closest Vertices')
-        
-        for i,line in enumerate(bands_as_lines):
-            xs = [line.point1[0],line.point2[0]]
-            ys = [line.point1[1],line.point2[1]]
-            ax.plot(xs,ys,color="black",linewidth=2)
-
-        ax.legend()
-        ax.set_axis_off()
-
-        fig2.savefig(f"{output_dir}/{image_name}_polygon.png")
-
-        plt.show()
         plt.waitforbuttonpress()
         plt.close()
     
