1ea8e113c039e3b7ce722388fc9bb08066966de1
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-07-25 09:44:53 +0300

    test reconstruction

--- src/my_http_client.py
+++ src/my_http_client.py
@@ -1,54 +1,40 @@
 import urllib3
 import json
-
 from urllib.parse import urlencode
 
 
-class HTTPClient():
-
-    def __init__(self,host="localhost",port=8888):
+class SpringsHTTPClient:
+    def __init__(self, host="localhost", port=8888):
         self.host = host
         self.port = port
         self.http = urllib3.PoolManager()
         self.url_prefix = "v0/RePAIR"
+        self.base_target = f"http://{self.host}:{self.port}/{self.url_prefix}"
 
     def send_sanity(self):
-        target = f"http://{self.host}:{self.port}/{self.url_prefix}/sanity"
-
+        target = f"{self.base_target}/sanity"
         response = self.http.request('GET', target)
 
         if response.status != 200:
-            raise response.reason
+            raise Exception(response.reason)
 
         return response.data.decode('utf-8')
-        
 
-    def send_reconstruct_request(self,body,screenshot_name="", api_version="v0"):
+    def send_reconstruct_request(self, body, screenshot_name=""):
+        query_parameters = {}
 
-        query_parameters = {
-            "noise":self.target_puzzle_noise,
-            "num":self.target_puzzle_num,
-            "dataset":"ConvexDrawing",
-            "image":self.target_puzzle_image
-        }
-
-        if screenshot_name !="":
+        if screenshot_name != "":
             query_parameters["screenShotName"] = screenshot_name
 
         encoded_args = urlencode(query_parameters)
-
-        query_parameters = "reconstructions?"+encoded_args
-        target = f"http://{self.host}:{self.port}/{api_version}/{query_parameters}"
+        query_parameters_str = "reconstructions?" + encoded_args
+        target = f"{self.base_target}/{query_parameters_str}"
 
         response = self.http.request(
-                    'POST',
-                    target,
-                    body=body,
-                    headers={'Content-Type': 'text/plain'}
-                )
-        
+            'POST',
+            target,
+            body=body,
+            headers={'Content-Type': 'text/plain'}
+        )
+
         return json.loads(response.data.decode('utf-8'))
-
-
-    
-
--- tests/test_http_client.py
+++ tests/test_http_client.py
@@ -3,15 +3,20 @@ sys.path.append("./")
 
 import unittest
 #from src.my_http_client import HTTPClient
-from src.my_http_client import HTTPClient 
+from src.my_http_client import SpringsHTTPClient 
 
 
 
 class TestHTTPClient(unittest.TestCase):
     
     def test_sanity(self):
-        http = HTTPClient()
+        http = SpringsHTTPClient()
         print(http.send_sanity())
+
+    def test_reconstruction(self):
+        http = SpringsHTTPClient()
+        res = http.send_reconstruct_request("RPf_00194_intact_mesh,1,RPf_00195_intact_mesh,1")
+        print(res)
 
 
 if __name__ == "__main__":
