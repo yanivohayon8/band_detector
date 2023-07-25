cc987b0209f0b791f8c02465c9dc60a38454fd84
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-07-25 12:07:09 +0300

    another test

--- tests/test_anchors_selectors.py
+++ tests/test_anchors_selectors.py
@@ -3,11 +3,17 @@ from src.pairwiser.anchors_selectors import RandomVertexSelector
 
 class TestAnchorsSelectors(unittest.TestCase):
 
-    def test_random_vertex(self):
+    def test_random_vertex_rpf_00194(self):
         rpf_00194_vertices = [15,16,16,17,32,33]
-        # rpf_00197_vertices =  [19,20,42,43,43,44]
 
         selector = RandomVertexSelector(rpf_00194_vertices)
+        sampled_indices =  selector.select_anchors()
+        print(sampled_indices)
+    
+    def test_random_vertex_rpf_00197(self):
+        rpf_00197_vertices =  [19,20,42,43,43,44]
+
+        selector = RandomVertexSelector(rpf_00197_vertices)
         sampled_indices =  selector.select_anchors()
         print(sampled_indices)
 
