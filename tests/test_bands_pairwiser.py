fdfe1aaadbe379a610da60cf40bb2f4633a75282
Yaniv Ohayon <yanivohayon8@gmail.com>
2023-08-05 14:13:48 +0300

    bands pairwise

--- tests/test_bands_pairwiser.py
+++ tests/test_bands_pairwiser.py
@@ -18,17 +18,31 @@ class TestPairwiser(unittest.TestCase):
         first_frag_anchor_1, first_frag_anchor_2 = anchor_selector.select_anchors(rpf_00194_closest_vertices)
         second_frag_anchor_1, second_frag_anchor_2 = anchor_selector.select_anchors(rpf_00197_closest_vertices)
         permutations = pairwise_permutations(first_frag_anchor_1,first_frag_anchor_2,second_frag_anchor_1,second_frag_anchor_2)
+        inner_springs_pairs_permutations = [[(0,0),(1,1)],[(0,1),(1,0)]]
+
 
         physical_assembler = PhysicalAssembler(SpringsHTTPClient())
 
         for i,permut in enumerate(permutations):
-            matings = []
-            matings.append(VertexMating(first_fragment,permut[0][0],second_fragment,permut[1][1]))
-            matings.append(VertexMating(first_fragment,permut[0][1],second_fragment,permut[1][0]))
-            print(matings[0])
-            print(matings[1])
-            response = physical_assembler.run(matings,screenshot_name=f"permutation_{i+1}")
-            print(f"permutation {i+1} score: ", physical_assembler.score_assembly(response))
+
+            for permut_spring_repr, spring_permutation in zip(["springPermA","springPermB"],inner_springs_pairs_permutations):
+                matings = []
+                first_spring_side1_i = spring_permutation[0][0]
+                first_spring_side2_i = spring_permutation[0][1]
+                second_spring_side1_i = spring_permutation[1][0]
+                second_spring_side2_i = spring_permutation[1][1]
+
+                mate_1 = VertexMating(first_fragment,permut[0][first_spring_side1_i],
+                                      second_fragment,permut[1][first_spring_side2_i])
+                matings.append(mate_1)
+
+                mate_2 = VertexMating(first_fragment,permut[0][second_spring_side1_i],
+                                      second_fragment,permut[1][second_spring_side2_i])
+                matings.append(mate_2)
+                print(matings[0])
+                print(matings[1])
+                response = physical_assembler.run(matings,screenshot_name=f"permutation_{i+1}_{permut_spring_repr}")
+                print(f"permutation {i+1} score: ", physical_assembler.score_assembly(response))
 
 
 if __name__ == "__main__":
